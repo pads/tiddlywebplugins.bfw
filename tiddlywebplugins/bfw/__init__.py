03bab301e9b5918c224c7fef559264a1f82d08c4
FND <fnd@localhost.localdomain>
2013-05-27 16:29:18 +0200

    moved initialization logic to separate module
    this avoids import weirdness while keeping `__init__.py` free of logic
    (so things like `setup.py` can use its metadata without worrying about
    side-effects or circular imports)

--- tiddlywebplugins/bfw/__init__.py
+++ tiddlywebplugins/bfw/__init__.py
@@ -11,27 +11,6 @@ __license__ = 'MIT'
 
 
 def init(config):
-    try:
-        selector = config['selector']
-    except KeyError: # twanager mode
-        return
+    from . import plugin
 
-    from tiddlywebplugins.utils import replace_handler
-    from . import web, middleware
-
-    config['server_response_filters'].insert(0, middleware.FriendlyError) # XXX: position arbitrary!?
-    selector.status404 = _error_handler('404 Not Found', 'not found')
-    selector.status405 = _error_handler('405 Method Not Allowed',
-            'method not allowed')
-
-    replace_handler(selector, '/', GET=web.frontpage)
-    selector.add('/~', GET=web.home)
-    selector.add('/register', POST=web.register_user)
-    selector.add('/logout', POST=web.logout)
-
-
-def _error_handler(status, message):
-    return lambda environ, start_response: (middleware.
-            render_error(environ, start_response, status, message=message))
-    # XXX: accessing `middleware:render_error` is hacky, particularly because it
-    #      relies on `middleware` having been imported by `init`
+    plugin.init(config)
