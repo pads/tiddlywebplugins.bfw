453203f1f6b396891bbd661c94249bef7f46a390
FND <fnd@localhost.localdomain>
2013-08-24 11:14:00 +0200

    activated wiki links

--- tiddlywebplugins/bfw/instance.py
+++ tiddlywebplugins/bfw/instance.py
@@ -4,5 +4,6 @@ basic structure and contents of a BFW
 
 instance_config = {
     'system_plugins': ['tiddlywebplugins.static', 'tiddlywebplugins.bfw'],
-    'twanager_plugins': ['tiddlywebplugins.bfw']
+    'twanager_plugins': ['tiddlywebplugins.bfw'],
+    'markdown.wiki_link_base': ''
 }
